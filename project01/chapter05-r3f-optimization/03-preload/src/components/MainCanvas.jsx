f6210514ac1cd81c67cc420f3db3f653d2cb1fae
junhee <limjunhee4575@gmail.com>
2024-07-14 01:04:42 +0900

    24/07/13

---
+++ project01/chapter05-r3f-optimization/03-preload/src/components/MainCanvas.jsx
@@ -0,0 +1,74 @@
+/* eslint-disable react/no-unknown-property */
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Html, useGLTF } from "@react-three/drei";
+import * as THREE from "three";
+import { TestMesh } from "./TestMesh";
+import { Suspense, useCallback, useEffect, useState } from "react";
+export const MainCanvas = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleModelLoaded = useCallback(() => {
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      handleModelLoaded();
+      console.log("로딩 끝!");
+    }, 5000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [handleModelLoaded]);
+  console.log("loading", isLoading);
+  return (
+    <Canvas
+      gl={{ antialias: true }}
+      camera={{
+        fov: 60,
+        aspect: window.innerWidth / window.innerHeight,
+        near: 0.1,
+        far: 100000,
+        position: [10, 10, 10],
+      }}
+      scene={{ background: new THREE.Color(0x000000) }}
+    >
+      <ambientLight intensity={2} />
+      <directionalLight position={[100, 100, 100]} intensity={2} />
+      <OrbitControls />
+      <Suspense
+        fallback={
+          <Html>
+            <div>로딩중...</div>
+          </Html>
+        }
+      >
+        {isLoading ? null : <TestMesh />}
+      </Suspense>
+    </Canvas>
+  );
+};
+
+useGLTF.preload([
+  "/dancer01.glb",
+  "/dancer02.glb",
+  "/dancer03.glb",
+  "/dancer04.glb",
+  "/dancer05.glb",
+  "/dancer06.glb",
+  "/dancer07.glb",
+  "/dancer08.glb",
+  "/dancer09.glb",
+  "/dancer10.glb",
+  "/dancer11.glb",
+  "/dancer12.glb",
+  "/dancer13.glb",
+  "/dancer14.glb",
+  "/dancer15.glb",
+  "/dancer16.glb",
+  "/dancer17.glb",
+  "/dancer18.glb",
+  "/dancer19.glb",
+  "/dancer20.glb",
+  "/dancer21.glb",
+]);
---
+++ project01/chapter05-r3f-optimization/03-preload/src/components/TestMesh.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/no-unknown-property */
+import { useGLTF } from "@react-three/drei";
+export const TestMesh = () => {
+  const gltfs = useGLTF([
+    "/dancer01.glb",
+    "/dancer02.glb",
+    "/dancer03.glb",
+    "/dancer04.glb",
+    "/dancer05.glb",
+    "/dancer06.glb",
+    "/dancer07.glb",
+    "/dancer08.glb",
+    "/dancer09.glb",
+    "/dancer10.glb",
+    "/dancer11.glb",
+    "/dancer12.glb",
+    "/dancer13.glb",
+    "/dancer14.glb",
+    "/dancer15.glb",
+    "/dancer16.glb",
+    "/dancer17.glb",
+    "/dancer18.glb",
+    "/dancer19.glb",
+    "/dancer20.glb",
+    "/dancer21.glb",
+  ]);
+  console.log(gltfs);
+  return (
+    <>
+      {gltfs.map((gltf, idx) => {
+        return (
+          <primitive
+            key={gltf.scene.id}
+            object={gltf.scene}
+            scale={0.1}
+            rotation-y={(Math.PI / 5) * idx}
+          />
+        );
+      })}
+    </>
+  );
+};
